84582a79a67402830fa40d8f6093226b066a4f1e
kaualira <130668344+kaualira@users.noreply.github.com>
2023-12-05 09:15:26 -0300

    comit converção de crlsius em fahrenheit

---
+++ Converção de Celsius em Fafrenheit.py
@@ -0,0 +1,7 @@
+# Converção de Celsius em Fahrenheit
+# Entrada
+Celsius = int(input("Incira o valor da tenperatua em Celsius:"))
+Fahrenheit = Celsius * (9/5) + 32
+
+print("Temperatura em Fahrenheit em: ", Fahrenheit)
+print("Invalido input")
